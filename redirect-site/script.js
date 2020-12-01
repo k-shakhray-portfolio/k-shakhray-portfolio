e2c93383c55844fa4abed69dcd6c14d8a7e955d6
Klim <shakhray3@gmail.com>
2020-12-01 20:12:32 +0200

    redirect-site

--- redirect-site/script.js
+++ redirect-site/script.js
@@ -8,12 +8,12 @@ if (referrer_url == 0) {
 }
 
 function redirect() {
-    if (referrer_url === valid_url) {
+    if (referrer_url == valid_url) {
         window.location.href = 'https://docs.google.com/document/d/1Ldr4qYKmmbhAMU1Zh-0Whq8V96cU3k-rdes5Qxt6ubE/edit'
     }
 }
 
-setTimeout(redirect, 1000);
+setTimeout(redirect, 10000);
 
 console.log(document.referrer)
 console.log(window.location.href)
