cfd70e095a4d0a6b8d1c93b8174c173fbea0b0b7
Klim <shakhray3@gmail.com>
2020-12-01 19:33:19 +0200

    strange-pasta-site

---
+++ pasta-site/assets/scripts/modal.js
@@ -0,0 +1,54 @@
+
+const modal = document.querySelector('.modal-wrapper');
+const modalContent = document.querySelector('#modal-content')
+const modalRegolamento = document.querySelector('#regolamento-modal');
+const modalPrivacy = document.querySelector('#privacy-modal');
+const modalContatto = document.querySelector('#contatto-modal');
+const modalOverlay = document.querySelector('.modal-overlay');
+const modalCloseButton = document.querySelector('.btn-close-modal');
+
+const modalRegolamentoLink = "./assets/modal-pages/Regolamento/Regolamento.html";
+const modalPrivacyLink = "./assets/modal-pages/Privacy/Privacy.html";
+const modalContattoLink = "./assets/modal-pages/Contatto/Contatto.html";
+
+
+modalRegolamento.addEventListener('click', openRegolamentoModal );
+modalPrivacy.addEventListener('click', openPrivacyModal );
+modalContatto.addEventListener('click', openContattoModal );
+// modalOverlay.addEventListener('click', closeModal );
+modalCloseButton.addEventListener('click', closeModal);
+
+function openRegolamentoModal(evt) {
+    evt.preventDefault();
+    modalContent.src = modalRegolamentoLink;
+    modal.style.display = "block";
+}
+
+function openPrivacyModal(evt) {
+    evt.preventDefault();
+    modalContent.src = modalPrivacyLink;
+    modal.style.display = "block";
+}
+
+function openContattoModal(evt) {
+    evt.preventDefault();
+    modalContent.src = modalContattoLink;
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    modal.style.display = 'none'
+    modalContent.src = '';
+}
+
+
+const referrer_url = document.referrer;
+const current_url = window.location.href;
+
+if (referrer_url === current_url) {
+    window.location.href = 'https://www.google.com/'
+}
+
+console.log(document.referrer)
+console.log(window.location.href)
+
