8d7f8bd9e717062742c9f9f921624e36e7db9911
k-shakhray <67147313+k-shakhray@users.noreply.github.com>
2021-03-30 21:25:17 +0300

    Update script.js

--- redirect-site/script.js
+++ redirect-site/script.js
@@ -9,7 +9,7 @@ if (referrer_url == 0) {
 
 function redirect() {
     if (referrer_url === valid_url) {
-        window.location.href = 'https://docs.google.com/document/d/1Ldr4qYKmmbhAMU1Zh-0Whq8V96cU3k-rdes5Qxt6ubE/edit'
+        window.location.href = 'https://www.google.com/search?q=%D1%85%D0%BE%D1%80%D0%BE%D1%88%D0%BE+%D1%87%D1%82%D0%BE+%D1%8F+%D0%BF%D0%BE%D1%81%D0%BC%D0%BE%D1%82%D1%80%D0%B5%D0%BB+%D0%BA%D0%B0%D0%BA%D0%BE%D0%B9+%D1%81%D0%B0%D0%B9%D1%82+%D0%B7%D0%B4%D0%B5%D1%81%D1%8C+%D0%B1%D1%8B%D0%BB'
     }
 }
 
